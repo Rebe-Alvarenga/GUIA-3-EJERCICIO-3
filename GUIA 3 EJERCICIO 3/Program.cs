1af3446143402eabda1204af5bbbcad7f910d279
Rebe-Alvarenga <rebequitasarai1@gmail.com>
2021-01-29 22:00:43 -0600

    Final Upload

--- GUIA 3 EJERCICIO 3/Program.cs
+++ GUIA 3 EJERCICIO 3/Program.cs
@@ -6,7 +6,21 @@ namespace GUIA_3_EJERCICIO_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+             *PROGRAMACION ESTRUCTURADA G07L
+             *REBECA SARAÍ ALVARENGA HERNÁNDEZ
+             *
+             *EJERCICIO #1 GUIA 3
+             */
+
+            double r, v;
+            Console.WriteLine("Bienvenido, te solicitare nada mas el valor del radio.");
+            Console.WriteLine("Ingrese por favor el radio...");
+            r = Math.Pow(Convert.ToDouble(Console.ReadLine()), 3);
+            v = 1.3333 * Math.PI * r;
+            Console.WriteLine("\nEl volumen de la esfera es de " + v);
+            Console.WriteLine("\nPresiona cualquier tecla para finalizar el programa...");
+            Console.ReadKey();
         }
     }
 }
